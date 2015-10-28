ba2e337154a49247f1b55d76680ecca821a1fc73
cwm262 <azathoth.hp@gmail.com>
2015-10-28 17:00:02 -0500

    Scaffolding

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+angular.module('cs4320aTeamApp'), []
+  .config(function($routeProvider){
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ scripts/controllers/main.js
@@ -0,0 +1,4 @@
+angular.module('cs4320aTeamApp')
+  .controller('MainCtrl', function($scope){
+    $scope.title = "CS4320 A Team App";
+  });
