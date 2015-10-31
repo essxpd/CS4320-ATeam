62ff9af8fe32e2e82b947d53b55d533fbaa3dd0e
cwm262 <azathoth.hp@gmail.com>
2015-10-31 05:31:08 +0000

    Made instruction set on applicant.html change based on ferpa score. Added some dummy data to show some UI elements. Added some view change functionality.

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,5 +1,37 @@
 angular.module('cs4320aTeamApp')
-  .controller('MainCtrl', function($scope){
-    $scope.applicant = "John Doe";
-    $scope.ferpa = "90";
+  .controller('MainCtrl', function($scope, $location, $http){
+  	/* Temporary applicant name and ferpa score to make UI skeleton look a little better
+	Be sure to Delete once real data is being brought in */
+	$scope.applicant = "John";
+	$scope.name = "John Doe";
+	$scope.ferpa = 91;
+	/* Delete above code once real data is brought in */
+
+	//Grab current URL
+	$scope.currentPath = $location.path();
+
+	//Change instruction menu based on ferpa score
+	if($scope.ferpa > 85){
+		$scope.CurrentInstructionSet = true;
+	};
+	
+	//On click, changes url to root/form, triggering a view change
+	$scope.goToForm = function(){
+   		$location.path('/form');
+	}
+
+	//If on form page, do this
+	if($scope.currentPath === '/form'){
+    		/*
+    		$http.get('/model/secLevels.php')
+    		.success(function(response){
+        		$scope.securityLevels = response.securityLevels;
+    		});
+    		*/
+    		var secLevels = [
+			{"number":"1", "questions":["quesiton1","question2"]},
+			{"number":"2", "questions":["question3","question4"]}
+		];
+		$scope.securityLevels = secLevels;
+	};
   });
