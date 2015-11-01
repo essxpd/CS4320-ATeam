cb1a5005ce9bd684df0357328cfda62bea87c662
cwm262 <azathoth.hp@gmail.com>
2015-11-01 03:38:28 +0000

    Added RequestType select option + studentworker checkbox to form. Added submit button to form. Added more dummy data to main.js. Implemented function takeFERPA. Began to add functionality for submit.

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,10 +1,17 @@
 angular.module('cs4320aTeamApp')
-  .controller('MainCtrl', function($scope, $location, $http){
+  .controller('MainCtrl', function($scope, $location, $http, $window){
   	/* Temporary applicant name and ferpa score to make UI skeleton look a little better
 	Be sure to Delete once real data is being brought in */
 	$scope.applicant = "John";
 	$scope.name = "John Doe";
-	$scope.ferpa = 91;
+	$scope.ferpa = 84;
+    $scope.DoB = new Date("September 1, 1939");
+    $scope.title = "Director";
+    $scope.dept = "The Dept";
+    $scope.paw = "jdoe39";
+    $scope.id = "11111111";
+    $scope.addr = "1111 East Broadway";
+    $scope.phoneNum = "(555)555-5555";
 	/* Delete above code once real data is brought in */
 
 	//Grab current URL
@@ -19,6 +26,9 @@ angular.module('cs4320aTeamApp')
 	$scope.goToForm = function(){
    		$location.path('/form');
 	}
+    $scope.takeFERPA = function(){
+        $window.location.href = "http://myzoutraining.missouri.edu/ferpa.html";
+    }
 
 	//If on form page, do this
 	if($scope.currentPath === '/form'){
@@ -28,14 +38,22 @@ angular.module('cs4320aTeamApp')
         		$scope.securityLevels = response.securityLevels;
     		});
     		*/
-        //Temp inclusion of a DoB to show something there in UI
-        $scope.DoB = new Date("September 1, 1939");
         
         //Temp dummy data for securityLevels object
         $scope.securityLevels = [
 			{"number":"1", "questions":["question1","question2"]},
 			{"number":"2", "questions":["question3","question4"]}
         ];
+        
+        /* To submit security level request data
+        $scope.saveRequest = function(){
+           $http.post('/model/saveRequest.php', {
+                requestType: $scope.requestType,
+                studentWorker: $scope.studentWorker,
+                secAnswers: $scope.securityLevels
+            }); 
+        }
+        */
         
 	};
   });
