1ac110c9555fe37cac9e1a67fce55e7e83bf400b
cwm262 <azathoth.hp@gmail.com>
2015-11-02 23:49:19 +0000

    Breaking login and index php temporarily to test operations

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <?php
+/*
         session_start();
 #        if(!$_SERVER['HTTPS'])
 #                header("Location: https://a-team.cloudapp.net/testProject/CS4320-ATeam/index.php");
         if (!isset($_SESSION['loggedIn']))
                 header("Location: http://a-team.cloudapp.net/testProject/CS4320-ATeam/login.php");
 #		will need to change testProject to Project before deployment
+*/
 ?>
 <html>
 <head>
