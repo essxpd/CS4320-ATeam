556f92854daaf7dcab5cd94fb19351950f68bc65
jbekcc <jbekcc@mail.missouri.edu>
2015-11-02 21:50:19 +0000

    added logout script to models/

--- model/logout.php
+++ model/logout.php
@@ -1,3 +1,13 @@
 <?php
+        session_start();
+#        if(!$_SERVER['HTTPS'])
+#                header("Location: https://a-team.cloudapp.net/Jeremy/CS4320-ATeam/model/login.php");
+        if (!isset($_SESSION['count']))
+                header("Location: http://a-team.cloudapp.net/Jeremy/CS4320-ATeam/login.php");
+
+        session_unset();
+        session_destroy();
+
+        header("Location: http://a-team.cloudapp.net/Jeremy/CS4320-ATeam/login.php");
 
 ?>
