ff88375c35579c202f9d92900d8deff41480396b
Christopher C Yaeger <ccyd6c@A-Team.A-Team.j6.internal.cloudapp.net>
2015-11-02 21:57:56 +0000

    adding the sql file for the database

---
+++ database.sql
@@ -0,0 +1,31 @@
+--A Team
+
+DROP SCHEMA IF EXISTS sqldb CASCADE;
+CREATE SCHEMA sqldb;
+SET search_path = sqldb;
+
+create table users (
+	Full_Name varchar(30),
+	Title varchar(15), /*things like student, teacher, etc...*/
+	Department varchar (30),
+	SSO varchar (15) PRIMARY KEY,
+	Employee_ID varchar (15),
+	Campus_Address varchar (50),
+	Phone_Number varchar(13), /*not sure if "()" or "-" will count in this so I made the max 13*/ 
+	Ferpa_Score varchar(10),
+	User_Type Varchar(15)
+);
+
+create table authentication (
+	SSO varchar(15) PRIMARY KEY,
+	Password_Hash char(40) NOT NULL;
+	Salt char(40) NOT NULL,
+	FOREIGN KEY (SSO) REFERENCES users(SSO)
+);
+
+create table log (
+	Registration_Date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	Active_Requests int,
+	Date_Approved date,
+	Request_Submitted date
+);
