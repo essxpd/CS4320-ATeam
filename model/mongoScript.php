522f5c7ad972f2ce55a0ee3944512615ac86f7d6
Mitchell Christian Battles <mcbxx2@A-Team.A-Team.j6.internal.cloudapp.net>
2015-11-02 23:01:05 +0000

    working RESTful mongo script

--- model/mongoScript.php
+++ model/mongoScript.php
@@ -6,19 +6,18 @@
 	// Connect to test database
 	$m = new Mongo("mongodb://$dbhost");
 	$db = $m->$dbname;
+	$dataTable = "forms";
 	// Operations
 	$method = $_SERVER['REQUEST_METHOD'];//$statusCode = $_POST['intake'];
 	if($method == 'GET'){
-		$dataTable = $_POST['table'];
+		$pawprint = $_GET['pawprint'];
 		$collection = $db->$dataTable;
 		$cursor = $collection->find();
 		$jsonArr = (json_encode(iterator_to_array($cursor)));
 		echo $jsonArr;
 	}else if($method == 'POST'){
-		$jsonData = $_POST['todoEs'];
-		$dataTable = $_POST['table'];
+		$jsonData = $_POST['data'];
 		$collection = $db->$dataTable;
-		$collection->remove(array());
 		$collection->insert($jsonData);
 		echo "inserted successfully";
 	}
