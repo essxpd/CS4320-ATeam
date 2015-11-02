46aa26615ee08f208a00132d1b0f80d67f50b349
jbekcc <jbekcc@mail.missouri.edu>
2015-11-02 18:31:41 +0000

    added login.php and modified main.js for login verification

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>CS4320 A-Team Login</title>
+  <meta name="viewport" content="width=device-width">
+
+  <!-- Main Style Sheet -->
+  <link rel="stylesheet" href="styles/main.css">
+
+  <!-- Get AngularJS -->
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js"></script>
+  <!-- Get Angular ngRoute -->
+  <script src="https://code.angularjs.org/1.4.7/angular-route.min.js"></script>
+
+  <script src="scripts/app.js"></script>
+  <script src="scripts/controllers/main.js"></script>
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+  
+</head>
+
+<body ng-app="cs4320aTeamApp" ng-controller="MainCtrl">
+<div id="login">
+	<form>
+		Username:<br>
+		<input type="text" ng-model="username">
+		<br>
+		Password:<br>
+		<input type="password" ng-model="password">
+		<br><br>
+		<button ng-click="login()">Submit</button>
+	</form>
+	<p>{{$scope.errorMsg}}</p>
+</div>
+<script>
+</script>	
+</body>
+
+</html>
--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,4 +1,37 @@
 angular.module('cs4320aTeamApp')
-  .controller('MainCtrl', function($scope){
-    $scope.title = "CS4320 A Team App";
-  });
+  .controller('MainCtrl', function($scope, $http){
+  
+$scope.login = function(){
+    var config = {
+        url: '/model/auth.php',
+        method: 'POST',
+        data: {
+            username: $scope.username,
+            password: $scope.password
+        },
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }
+    $http(config)
+    .success(function(data, status, headers, config){
+        if(data.status){
+            $scope.fname = data.fname;
+            $scope.lname = data.lname;
+            $scope.fullName = data.fname + " " + data.lname;
+            $scope.ferpa = data.ferpa;
+            $scope.dob = data.dob;
+            $scope.title = data.title;
+            $scope.dept = data.dept;
+            $scope.empID = data.empID;
+            $scope.campAddr = data.campAddr;
+            $scope.phoneNum = data.phoneNum;
+            $location.path('/');
+        } 
+        else{
+            $scope.errorMsg = 'Login has failed. Please check your name and password then try again.';
+        }
+    })
+    .error(function(data, status, headers, config){
+        $scope.errorMsg = "There has been a problem with your login. Please contact so&so@mizzou.edu for support."
+    });
+};
+});
