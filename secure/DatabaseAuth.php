fc9854b0da642ac912ebaee3f574c7215e522379
cwm262 <azathoth.hp@gmail.com>
2015-11-04 02:12:34 +0000

    readding dbconnect.php with secure parts removed. removing DatabaseAuth.php for security purposes

--- secure/DatabaseAuth.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-//$servername = "localhost";
-//$username = "root";
-//$password = "Ateam4320";
-	DEFINE("SERVERNAME","localhost");
-	DEFINE("USERNAME","root");
-	DEFINE("PASSWORD","Ateam4320");
-?>
