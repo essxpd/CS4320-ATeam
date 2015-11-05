6a7d2fb8c180ca8ea77fe2fa9f109fff6ae532c0
ccyd6c <ccyd6c@mail.missouri.edu>
2015-11-05 01:33:27 +0000

    made a json variable in auth.php with all user data

--- model/auth.php
+++ model/auth.php
@@ -39,6 +39,21 @@ require("dbconnect.php");
 						else
 							$_SESSION["loggedIn"] = 1;//for applicants
 						$_SESSION["User_Type"] = $user_type;
+						$SSO = $_SESSION["SSO"];
+						$sql = "SELECT * FROM testAuth.users WHERE sso = " . $SSO;
+						$result = mysqli_query($conn, $sql);
+
+						if ((mysqli_num_rows($result)) > 0) {//making sure something was found
+							$row = mysqli_fetch_assoc($result);
+							//puts all biodata into an array
+							$arr = array('Full_Name' => $row["Full_Name"], 'Title' => $row["Title"], 'Department' => $row["Department"], 'SSO' => $row["SSO"], 'Employee_ID' => $row["Employee_ID"],
+									'Campus_Address' => $row["Campus_Address"], 'Phone_Number' => $row["Phone_Number"], 'Ferpa_Score' => $row["Ferpa_Score"], 'User_Type' => $row["User_Type"]);
+							//puts array into json object
+							$BioData = json_encode($arr);
+							//echo $BioData;//send this wherever you want
+						} else {//error message change to whatever you want
+							echo "0 results";
+						}
                 }
                 else
                         exit("Invalid login credentials");
