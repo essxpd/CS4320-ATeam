dd6d9c937b7027118e1a1eda5b09e3311127fa99
mcbxx2 <mcbxx2@mail.missouri.edu>
2015-11-08 18:25:50 +0000

    added a department search functionality to the mongoScript

--- model/mongoScript.php
+++ model/mongoScript.php
@@ -11,8 +11,13 @@
 	$method = $_SERVER['REQUEST_METHOD'];//$statusCode = $_POST['intake'];
 	if($method == 'GET'){
 		$pawprint = $_GET['pawprint'];
+		$department = $_GET['department'];
 		$collection = $db->$dataTable;
-		$searchArray = array('paw' => $pawprint);
+		if($department =! NULL){
+			$searchArray = array('dept' => $department);
+		}else{
+			$searchArray = array('paw' => $pawprint);
+		}
 		$cursor = $collection->find($searchArray);
 		$jsonArr = (json_encode(iterator_to_array($cursor)));
 		echo $jsonArr;
