a2d03cd665519a2c01507f2d4fb8b35c27ea3ecf
ccyd6c <ccyd6c@mail.missouri.edu>
2015-11-10 00:11:21 +0000

    put some sample questions into makePDF

--- model/makePDF.php
+++ model/makePDF.php
@@ -1,8 +1,34 @@
 <?PHP
 require_once('../plugins/mpdf60/mpdf.php');
+session_start();
 $mpdf = new mPDF();
 $test = 'test variable';
-$mpdf->WriteHTML('<p>Put whatever html you want here and add variables like this: ' . $test . '.</p>');
+$arr = $_SESSION['BioData'];
+
+
+
+
+
+
+$mpdf->WriteHTML('
+	<p>Name: ' . $arr['Full_Name'] . '.</p>
+	<p>Title: ' . $arr['Title'] . '.</p>
+	<p>Department: ' . $arr['Department'] . '.</p>
+	<p>SSO: ' . $arr['SSO'] . '.</p>
+	<p>Employee ID: ' . $arr['Employee_ID'] . '.</p>
+	<p>Campus Address: ' . $arr['Campus_Address'] . '.</p>
+	<p>Phone Number: ' . $arr['Phone_Number'] . '.</p>
+	<p>Ferpa Score: ' . $arr['Ferpa_Score'] . '.</p>
+	');
+$copystaffmember = false;
+if ($copystaffmember == true) {//I will change these variables to what actually gets sent back from mongo
+	$mpdf->WriteHTML('
+	<p>New Request: ' . $newRequest . '.</p>
+	<p>Additional Request: ' . $addRequest . '.</p>
+	<p>Student Worker: ' . $studentWorker . '.</p>
+	');
+}
+
 $mpdf->Output();
 exit;
 ?>
